5776a5360e4eaddfa893963c148ef47043e4326b
Алексей Тарасов <6238291@gmail.com>
2022-08-21 12:17:11 +0300

    правки ревьюера в views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -115,25 +115,30 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (AuthorAdminModeratorOrReadOnly,)
 
+    def define_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get("title_id"))
+
     def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
+        title = self.define_title()
         new_queryset = title.reviews.all()
         return new_queryset
 
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
+        title = self.define_title()
         serializer.save(author=self.request.user, title=title)
 
 
-class CommentsViewSet(viewsets.ModelViewSet):
+class CommentsViewSet(ReviewViewSet):
     serializer_class = CommentsSerializer
-    permission_classes = (AuthorAdminModeratorOrReadOnly,)
+
+    def define_review(self):
+        return get_object_or_404(Review, id=self.kwargs.get("review_id"))
 
     def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
+        review = self.define_review()
         new_queryset = review.comments.all()
         return new_queryset
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, id=self.kwargs.get("review_id"))
+        review = self.define_review()
         serializer.save(author=self.request.user, review=review)
